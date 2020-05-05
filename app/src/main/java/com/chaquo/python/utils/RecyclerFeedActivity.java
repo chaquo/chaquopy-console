a04b14f2519bd2efc894342c462d770e29e35e06
Travis Rivera Petit <rptravis96@gmail.com>
2020-05-05 15:16:25 +0200

    Add beautiful pop up

--- app/src/main/java/com/chaquo/python/utils/RecyclerFeedActivity.java
+++ app/src/main/java/com/chaquo/python/utils/RecyclerFeedActivity.java
@@ -6,7 +6,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Application;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import com.chaquo.python.console.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -16,6 +22,36 @@ public class RecyclerFeedActivity extends BacNetActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+
+
+    public void openSendMessageActivity(){
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.activity_send_message, null);
+
+        // create the popup window
+        //int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int width = 800;
+        //int height = 950;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(recyclerView, Gravity.CENTER, 0, -100);
+
+        // dismiss the popup window when touched
+        /*
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
+         */
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +63,9 @@ public class RecyclerFeedActivity extends BacNetActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                openSendMessageActivity();
             }
         });
-
-
-
-
-
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
